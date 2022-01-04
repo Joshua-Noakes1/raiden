002d496d42cfe47abc73f400f669fdae22f32545
Joshua Noakes <joshua.noakes1@icloud.com>
2022-01-04 15:47:10 +0000

    added fix for docker and unix in not loading gifs

--- commands/tiktok/lib/videoLib/webpToGIF.js
+++ commands/tiktok/lib/videoLib/webpToGIF.js
@@ -1,11 +1,12 @@
 const lcl = require('cli-color'),
     {
+        exec,
         spawn
     } = require('child_process');
 
 async function webpToGIF(webpPath, webpUUID, gifPath) {
     try {
-        await spawn('magick', [webpPath, gifPath]);
+        await spawn(process.platform == 'win32' ? 'magick' : 'convert', [webpPath, gifPath]);
         console.log(lcl.green("[WebpToGIF - Success]"), `Converted webp ("${webpUUID}.webp") to gif`);
         return {
             path: gifPath,
