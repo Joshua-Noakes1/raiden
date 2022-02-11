85670aaca64fc9242af977469f94244fbb867885
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-11 16:40:44 +0000

    fix for vm.tiktok.com urls

--- bin/ytdlp/bin/ytdlpExec.js
+++ bin/ytdlp/bin/ytdlpExec.js
@@ -1,11 +1,25 @@
 const lcl = require('cli-color'),
+    fetch = require('node-fetch'),
     path = require('path'),
     {
         create: createYoutubeDl
     } = require('youtube-dl-exec'),
     ytdlp = createYoutubeDl(path.join(__dirname, process.platform == 'win32' ? 'ytdlp-exec.exe' : 'ytdlp-exec'));
 
-async function execDownload(videoURL) {
+async function execDownload(url) {
+    /* fix for vm.tiktok.com timeout - Could not send HEAD request to https://vm.tiktok.com/ZMLje4Gem/:
+        This should be temporary, until i get time to put a proper fix in place
+    */
+    if (url.includes('vm')) {
+        var videoURL = await fetch(url, {
+            method: 'HEAD',
+            redirect: 'manual'
+        });
+        videoURL = await videoURL.headers.get('Location');
+    } else {
+        var videoURL = url;
+    }
+
     try {
         // get video info
         var videoResult = await ytdlp(videoURL, {
