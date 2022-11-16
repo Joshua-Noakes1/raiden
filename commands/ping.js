6a9f8a45ddfea2c983f3efaff882c982b70713d7
Joshua Noakes <joshua.noakes1@icloud.com>
2022-11-16 20:44:25 +0000

    add ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,35 @@
 const {
-    SlashCommandBuilder
+    SlashCommandBuilder,
+    EmbedBuilder
 } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Pong!'),
+        .setDescription('See Raiden\'s latency.'),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        // random array of hex pastels
+        var colors = ["#FFB7B2", "#FFDAC1", "#E2F0CB", "#B5EAD7", "#C7CEEA"];
+        var color = colors[Math.floor(Math.random() * colors.length)];
+
+        // build embed
+        const embed = new EmbedBuilder()
+            .setTitle('Pong! 🏓')
+            .addFields({
+                name: "Discord API Latency",
+                value: `${interaction.client.ws.ping}ms`,
+                inline: true
+            }, {
+                name: "Message Latency",
+                value: `${Date.now() - interaction.createdTimestamp}ms`,
+                inline: true
+            })
+            .setColor(color)
+            .setTimestamp();
+
+        await interaction.reply({
+            embeds: [embed],
+            ephemeral: true
+        });
     }
 }
