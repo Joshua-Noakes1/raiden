871ecad2aaff23950ec24309f479f647a562d0b7
Joshua Noakes <joshua.noakes1@icloud.com>
2022-11-18 15:32:48 +0000

    add videoObject

--- commands/video.js
+++ commands/video.js
@@ -2,6 +2,7 @@ const lcl = require('cli-color');
 const colorHex = require('../lib/color');
 const getTikTok = require('../lib/fetchTikTok');
 const downloadMedia = require('../lib/downloadMedia');
+const getTime = require('../lib/getTime');
 const {
     SlashCommandBuilder,
     EmbedBuilder
@@ -42,6 +43,7 @@ module.exports = {
 
         try {
             // check URL
+            console.log(lcl.blue("[Video - Info]"), "Checking URL...");
             var url = interaction.options.getString('url').toString();
             if (url.match(/^(?:https?:\/\/)?(?:vm\.)?(?:www\.)?tiktok\.com/gm) == null || url == null || url == undefined || url == "") {
                 const errorEmbed = new EmbedBuilder() // a68765ba-b7ad-43bb-9c3a-748e7bafba7e
@@ -54,6 +56,7 @@ module.exports = {
                 });
                 return;
             }
+            console.log(lcl.green("[Video - Success]"), "URL is valid.");
 
             // create initial embed - 327fe4d3-0986-435e-8d86-59785d86cde6
             const videoInitialEmbedP1 = new EmbedBuilder()
@@ -76,6 +79,8 @@ module.exports = {
                 ephemeral: true
             });
 
+            // get TikTok
+            console.log(lcl.blue("[Video - Info]"), "Getting TikTok...");
             var tikTokVideo = await getTikTok(url);
             if (!tikTokVideo.success) {
                 // tiktok ytdlp failure - ee128fe2-bc0e-41a8-95b7-4872e32265f1
@@ -89,11 +94,59 @@ module.exports = {
                 });
                 return;
             }
+            console.log(lcl.green("[Video - Success]"), "Found TikTok.");
 
-            var mediaTest = await downloadMedia(tikTokVideo.video.formats[0].url, tikTokVideo.video.formats[0].video_ext);
-            console.log(mediaTest);
+            // build video object
+            var videoObject = {
+                "account": {
+                    "name": `${tikTokVideo.video.creator}`,
+                    "username": `${tikTokVideo.video.uploader}`,
+                    "id": `${tikTokVideo.video.uploader_id}`,
+                    "uuid": `${tikTokVideo.video.uploader_url.toString().split("/@")[1]}`,
+                    "url": `${tikTokVideo.video.uploader_url}`,
+                },
+                "video": {
+                    "id": `${tikTokVideo.video.id}`,
+                    "description": `${tikTokVideo.video.title.toString().replace(/\s+$/, '')}`,
+                    "url": `${tikTokVideo.video.webpage_url}`,
+                    "uploadTime": await getTime(Math.floor(tikTokVideo.video.timestamp * 1000)),
+                    "stats": {
+                        "views": `${tikTokVideo.video.view_count}`,
+                        "likes": `${tikTokVideo.video.like_count}`,
+                        "comments": `${tikTokVideo.video.comment_count}`,
+                        "reposts": `${tikTokVideo.video.repost_count}`,
+                    },
+                    "media": {
+                        "watermark": {
+                            "url": "",
+                            "filesize": 0, // In Bytes
+                            "format": ""
+                        },
+                        "clean": {
+                            "url": "",
+                            "filesize": 0, // In Bytes
+                            "format": ""
+                        }
+                    },
+                    "thumbnail": {
+                        "dynamic": {
+                            "url": "",
+                            "format": ""
+                        },
+                        "static": {
+                            "url": "",
+                            "format": ""
+                        }
+                    }
+                }
+            }
 
-            console.log(tikTokVideo);
+            console.log(videoObject);
+
+            // var mediaTest = await downloadMedia(tikTokVideo.video.formats[0].url, tikTokVideo.video.formats[0].video_ext);
+            // console.log(mediaTest);
+
+            // console.log(tikTokVideo);
 
         } catch (err) {
             // something went wrong - 5d36cc82-cd48-4a8b-b859-5ddffb7a63f2
